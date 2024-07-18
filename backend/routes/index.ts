f2d2bd863ed3bad67abcda76480dc80fefaa54ef
ThadeuJose <thadeujs@gmail.com>
2024-07-18 18:16:28 -0300

    Add basic eslint and prettier configs to backend

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,23 +1,22 @@
-import {Router,  Request, Response } from "express";
-import { getAllMovies } from "../api";
+import { Router, Request, Response } from 'express';
+import { getAllMovies } from '../api';
 
-const router:Router = Router();
+const router: Router = Router();
 
-router.get('/', function(req: Request, res: Response) {
-    getAllMovies().then(function (response) {             
-        res.send(response.data.results.map(formatData))
-    })
+router.get('/', function (req: Request, res: Response) {
+  getAllMovies().then(function (response) {
+    res.send(response.data.results.map(formatData));
+  });
 });
 
-function formatData(data:any){
-    const IMAGE_URL = `https://image.tmdb.org/t/p/w300`;
-    return {
-        id: data.id,
-        title: data.title,
-        vote: parseFloat(data.vote_average.toFixed(1)),
-        image: IMAGE_URL + data.poster_path
-    }
-
+function formatData(data: any) {
+  const IMAGE_URL = `https://image.tmdb.org/t/p/w300`;
+  return {
+    id: data.id,
+    title: data.title,
+    vote: parseFloat(data.vote_average.toFixed(1)),
+    image: IMAGE_URL + data.poster_path,
+  };
 }
 
 export default router;
